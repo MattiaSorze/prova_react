f213bc837abec1663f7f3038754187cffdfa37ec
U0K7074 <mattia.sorze@esternibisp.com>
2022-02-07 18:39:42 +0100

    changed from component to class

--- src/app.jsx
+++ src/app.jsx
@@ -1,34 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 import Layout from "./components/layout";
 
-import {makeStyles} from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-    item: {
-        padding: "5px"
-    },
-    space:{
-        paddingBottom: "50px"
-    },
-    textArea: {
-        width: "30%",
-        textAlign: "left"
-    },
-    button: {
-        padding: "5px",
-        minWidth: "150px"
-    },
-    typography: {
-        visibility: "hidden"
-    }
-}));
-
-const App = () => {
-    return (
+class App extends Component {
+    render(){
+      return (
         <React.Fragment>
-            <Layout/>
+          <Layout/>
         </React.Fragment>
-    );
+      );
+    }
 };
 
 export default App;
